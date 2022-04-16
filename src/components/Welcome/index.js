0533df80d84adcc3be54452e77a844332e2a1f38
Luiz Silva <luizsilva.ee@gmail.com>
2022-04-16 17:43:00 -0300

    feat(welcome): Create welcome section

---
+++ src/components/Welcome/index.js
@@ -0,0 +1,24 @@
+import './styles.css';
+import welcomeImg from '../../assets/developer.svg';
+
+const Welcome = () => {
+  return (
+    <section id="welcome-section">
+      <div className="section-container">
+        <div className="left">
+          <h1>Olá, sou Luiz Silva</h1>
+          <div className="subtext">Desenvolvedor web autodidata</div>
+          <p>
+            Potiguar, trabalho como desenvolvedor desde 2020. Atualmente
+            atuo como dev fullstack na <a href="http://negocioonlinedozero.com" target="_blank">NODZ</a>.
+          </p>
+        </div>
+        <div className="right">
+          <img src={welcomeImg} alt="Desenvolvedor" srcset="" />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Welcome;
